66d87ab6154738d690ed215df205b8531ed769f3
Marvin G. | DotNetExe <marvin.gorny99@gmail.com>
2024-12-01 11:08:52 +0100

    AOC 2024 - Day 01

---
+++ AOC2024/Day01/TotalDistance.cs
@@ -0,0 +1,77 @@
+﻿namespace AOC2024.Day01;
+
+public static class TotalDistance
+{
+    public static int[][] ReadInTestData(string path)
+    {
+        var resultingArray = File.ReadAllLines(path)
+            .Select(line => line.Split(" ")
+                .Where(x => x.Length > 0)
+                .Select(int.Parse)
+                .ToArray())
+            .ToArray();
+        
+        var rows = Enumerable.Range(0, resultingArray.GetLength(0))
+            .Select(i => new int[] { resultingArray[i][0], resultingArray[i][1] })
+            .ToArray();
+        
+        var sortedRows = rows
+            .OrderBy(row => row[0])
+            .ThenBy(row => row[1])
+            .ToArray();
+        
+        var secondColumn = sortedRows
+            .Select(row => row[1])
+            .OrderBy(x => x)
+            .ToArray();
+        
+        for (var i = 0; i < sortedRows.Length; i++)
+            sortedRows[i][1] = secondColumn[i];
+        
+        for (var i = 0; i < sortedRows.Length; i++)
+        {
+            resultingArray[i][0] = sortedRows[i][0];
+            resultingArray[i][1] = sortedRows[i][1];
+        }
+
+        return resultingArray;
+    }
+    
+    /// <summary>
+    /// Part 01
+    /// </summary>
+    /// <param name="input"></param>
+    public static void CalculateDistance(int[][] input)
+    {
+        var sum = input.Select(row => row)
+            .Select(x => Math.Abs(x[0] - x[1]))
+            .Sum();
+        
+        Console.WriteLine($"Part01 - {sum}");
+    }
+    
+    /// <summary>
+    /// Part 2
+    /// </summary>
+    /// <param name="input"></param>
+    public static void CalculateSimilarity(int[][] input)
+    {
+        var similarities = new List<int>();
+
+        var secondCol = Enumerable.Range(0, input.GetLength(0))
+            .Select(i => input[i][1] )
+            .ToArray();
+        
+        for (var row = 0; row < input.GetLength(0); row++)
+        {
+            var leftCol = input[row][0];
+            var amountInRight = secondCol.Count(x => x == leftCol);
+
+            similarities.Add(leftCol * amountInRight);
+        }
+
+        var sum = similarities.Sum();
+        
+        Console.WriteLine($"Part02 - {sum}");
+    }
+}
---
+++ AOC2024/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using AOC2024.Day01;
+
+namespace AOC2024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        if (directory == null)
+            throw new IOException("Directory could not be found");
+
+        const bool isExample = false;
+        var path = Path.Combine(directory, "Day01", "Datasets", isExample ? "example.txt" :  "day01_data.txt");
+        
+        var inputData = TotalDistance.ReadInTestData(path);
+        
+        TotalDistance.CalculateDistance(inputData);
+        TotalDistance.CalculateSimilarity(inputData);
+    }
+}
